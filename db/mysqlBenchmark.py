498a5aa7b2e1770ed1c031592e930576f0b0ffe1
Leo Kotschenreuther <leo.kotschenreuther@student.hpi.uni-potsdam.de>
2014-12-08 13:21:06 +0100

    Separated mysql and postgis basic file from benchmark file; First commit importExport tables

---
+++ db/mysqlBenchmark.py
@@ -0,0 +1,109 @@
+import mysql
+
+class MySQLBenchmark:
+	# @Todo: Basci MySQL einbinden
+	self.cursor.execute('SET PROFILING = 1')
+	def pointString(self, point):
+		return "Point(" + str(point['x']) + " " + str(point['y']) + ")"
+
+	def polygonString(self, polygon):
+		string = "Polygon(("
+		for point in polygon:
+			string += str(point['x']) + " " + str(point['y']) + ","
+		string += str(polygon[0]['x']) + " " + str(polygon[0]['y']) + "))"
+		return string
+
+	def lineString(self, line):
+		string = "Linestring("
+		for point in line:
+			string += str(point['x']) + " " + str(point['y']) + ","
+		string = string[:-1] + ")"
+		return string
+
+	def dropCreateTable(self, table):
+		dropTable = "DROP TABLE IF EXISTS " + table
+		self.cursor.execute(dropTable)
+		print("\tDropped Table")
+		createTable = ""
+		if table == 'POLYGONS':
+			createTable = "CREATE TABLE " + table + " (ID integer, size integer, polygon GEOMETRY)"
+		elif table == 'B_LINES':
+			createTable = "CREATE TABLE " + table + " (ID integer, size integer, line GEOMETRY)"
+		elif table == 'B_POINTS':
+			createTable = "CREATE TABLE " + table + " (ID INTEGER, X FLOAT, Y FLOAT, point POINT)"
+		self.cursor.execute(createTable)
+		self.db.commit()
+		print("\tCreated Table")
+
+	def insertPolygons(self, polygons, offset):
+		for i, polygon in enumerate(polygons):
+			size = len(polygon)
+			insert = '''INSERT INTO POLYGONS (ID, SIZE, polygon) VALUES (%s, %s, PolygonFromText(%s))'''
+			self.cursor.execute(insert, (i + offset, size, self.polygonString(polygon)))
+			if i % 1000 == 999:
+				print "finished: " + str(i+1)
+		self.db.commit()
+		print("\tInserted Polygons into polygons table")
+
+	def insertLines(self, lines, offset):
+		for i, line in enumerate(lines):
+			size = len(line)
+			insert = '''INSERT INTO B_LINES (ID, SIZE, line) VALUES (%s, %s, LineStringFromText(%s))'''
+			self.cursor.execute(insert, (i + offset, size, self.lineString(line)))
+			if i % 1000 == 999:
+				print "finished: " + str(i+1)
+		self.db.commit()
+		print("\tInserted Lines into lines table")
+
+	def insertPoints(self, points):
+		for i, point in enumerate(points):
+			# print self.pointString(point)
+			insert = '''INSERT INTO B_POINTS (ID, X, Y, POINT) VALUES (%s, %s, %s, PointFromText(%s))'''
+			self.cursor.execute(insert, (i, point['x'], point['y'], self.pointString(point)))
+			if i % 1000 == 999:
+				print "finished: " + str(i+1)
+		self.db.commit()
+		print("\tInserted Points into Points table")
+
+	def runQueries(self, queries, numberOfExecutions):
+		allQueries = len(queries) * numberOfExecutions
+		n = 0
+		results = {'database': 'MySQL', 'queries': list()}
+		for query in queries:
+			results['queries'].append({'name': query, 'times': list(), 'avg': 0})
+			for x in range(0, numberOfExecutions):
+				n = n + 1
+				self.cursor.execute(query)
+				if n % 15 == 0:
+					self.cursor.execute('SHOW PROFILES')
+					for row in self.cursor:
+						queryObject = {}
+						for x in results['queries']:
+							if x['name'] == row[2]:
+								queryObject = x
+								break
+						queryObject['times'].append(1000 * row[1])	# Time in Milliseconds
+				if n % (math.ceil(allQueries/10.0)) == 0:
+					print('\tFinished: ' + str(n * 100.0 / allQueries) + '%')
+		if n % 15 != 0:
+			self.cursor.execute('SHOW PROFILES')
+			for x, row in enumerate(self.cursor):
+				if n < 15 or x >= 15 - (n % 15):
+					queryObject = {}
+					for x in results['queries']:
+						if x['name'] == row[2]:
+							queryObject = x
+							break
+					queryObject['times'].append(1000 * row[1])		# Time in Milliseconds
+
+		for query in results['queries']:
+			avg = 0
+			x = 0.0
+			for val in query['times']:
+				avg = avg + val
+				x = x + 1
+
+			avg = avg / x
+			query['avg'] = avg
+
+		return results
---
+++ importexport.py
@@ -0,0 +1,100 @@
+import argparse
+import sys
+import re
+from db import mysql, postgis
+
+supported_databases = ("mysql", "postgis", "postgresql")
+
+def loadTableNames(tablesfile):
+	return [line.rstrip() for line in tablesfile]
+
+def importExport(source_db, destination_db, tableNames):
+	in_db = None
+	out_db = None
+	if source_db == "mysql":
+		in_db = mysql.MySQL()
+	# elif input_db == "postgis" or input_db == "postgresql":
+	# 	in_db = postgis.Postgis()
+	else:
+		sys.exit(2)
+
+	if destination_db == "mysql":
+		# out_db = mysql.MySQL()
+		sys.exit(2)
+	elif destination_db == "postgis" or destination_db == "postgresql":
+		out_db = postgis.Postgis()
+	else:
+		sys.exit(2)
+
+	for table in tableNames:
+		show_columns = "SHOW COLUMNS FROM " + table
+		in_db.cursor.execute(show_columns)
+		columns = []
+		for row in in_db.cursor:
+			columns.append({'name': row[0], 'type': row[1], 'null': row[2], 'key': row[3]})
+
+		try:
+			dropTable = "DROP TABLE " + table
+			out_db.cursor.execute(dropTable)
+			out_db.connection.commit()
+			
+		except:
+			"table " + table + " doesn't exist"
+
+		createTable = "CREATE TABLE " + table + " ("
+		for column in columns:
+			createTable += column['name']
+			int_pattern = re.compile("int")
+			double_pattern = re.compile("double")
+			if int_pattern.match(column['type']):
+				createTable += ' ' + 'integer'
+			elif double_pattern.match(column['type']):
+				createTable += ' decimal'
+			else:
+				createTable += ' ' + column['type']
+			if column['key'] == 'PRI': createTable += ' ' + 'PRIMARY KEY'
+			if column['null'] == 'NO': createTable += ' ' + 'NOT NULL'
+			createTable += ', '
+		createTable = createTable[:-2] + ")"
+		# print createTable
+
+		out_db.cursor.execute(createTable)
+		out_db.connection.commit()
+
+		selectAll = "SELECT "
+		for column in columns:
+			if column['type'] == "geometry":
+				selectAll += "AsText(" + column['name'] + "), "
+			else:
+				selectAll += column['name'] + ', '
+		selectAll = selectAll[:-2] + " from " + table + " limit 1000"
+		in_db.cursor.execute(selectAll)
+		for row in in_db.cursor:
+			insert = "INSERT INTO " + table + " ("
+			for column in columns:
+				insert += column['name'] + ", "
+			insert = insert[:-2] + ") VALUES ("
+			for column in columns:
+				if column['type'] == "geometry":
+					insert += "ST_GeomFromText(%s, 4326), "
+				else:
+					insert += "%s, "
+			insert = insert[:-2] + ")"
+			out_db.cursor.execute(insert, row)
+
+		out_db.connection.commit()
+		print "Finished table " + table 
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Export geometry tables from one database to another.')
+	parser.add_argument('source_db', help="The name of the database where you want to export the tables from.")
+	parser.add_argument('destination_db', help="The name of the database where you want to export the tables to.")
+	parser.add_argument('file', type=argparse.FileType('r'), help="A file that contains all table names you want to export.")
+	args = parser.parse_args()
+	if args.source_db not in supported_databases or args.destination_db not in supported_databases:
+		print "One of your databases is not supported. Try one of the following ones:"
+		for database in supported_databases:
+			print " * " + database
+		sys.exit(2)
+	tableNames = loadTableNames(args.file)
+	importExport(args.source_db, args.destination_db, tableNames)
